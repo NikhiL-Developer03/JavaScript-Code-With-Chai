a5b8f129d439644b687aa9e2d74ac3670df2e321
Nikhil.Developer03 <nikhil.developer03@gmail.com>
2024-07-28 15:16:56 +0530

    add javaScript files

---
+++ app.js
@@ -0,0 +1,29 @@
+// object
+let animal = {
+    name : "zebra",
+    leg : 3
+};
+
+// console.log(animal);
+
+// for in...
+// for(let key in animal){
+//     console.log(key,animal[key]);
+// }
+
+//Array
+let names = ["apple","mango","tomato","carrot"];
+// for in...
+// for(let index in names){
+//     console.log(index,names[index]);
+// }
+
+// for....of
+// for(let myvar of names){
+//     console.log(myvar);
+// }
+
+// console.log("hello world");
+
+
+console.log(a);var a;
